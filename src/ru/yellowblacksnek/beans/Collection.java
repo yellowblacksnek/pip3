3ee0f79ebdd94e1f298a0f274ef785715759dc6a
yellowblacksnek <truemarat@gmail.com>
2019-11-26 03:43:48 +0300

    замена deprecated кода

--- src/ru/yellowblacksnek/beans/Collection.java
+++ src/ru/yellowblacksnek/beans/Collection.java
@@ -3,14 +3,17 @@ package ru.yellowblacksnek.beans;
 import org.hibernate.Session;
 import ru.yellowblacksnek.db.HibernateSessionFactory;
 
-import javax.faces.bean.ManagedBean;
+import javax.persistence.criteria.CriteriaQuery;
 import java.util.List;
 
-@ManagedBean
 public class Collection {
     public List<Point> getPoints(){
         Session session = HibernateSessionFactory.getSessionFactory().openSession();
-        List<Point> points = session.createCriteria(Point.class).list();
+
+        CriteriaQuery<Point> criteriaQuery = session.getCriteriaBuilder().createQuery(Point.class);
+        criteriaQuery.from(Point.class);
+
+        List<Point> points = session.createQuery(criteriaQuery).getResultList();
         return points;
     }
 
